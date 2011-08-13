affc1166448e2f5d0a52e78ea9470c36e4de349e
Global Extensions <dukeglobex@gmail.com>
2011-08-13 15:55:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '1d57e9bf3b7c6800572214887e2b18ee4069756c427c1f63d112c6fb499e22f7a192914aad7fc70f106c0e4fae59919a7ce484d941fc50b6b51c6c15c55771ee'
